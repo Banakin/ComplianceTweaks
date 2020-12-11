74058d6f5f813816abc4cfe7b39471d9ffd3aad3
Brendan Leu <banakin9@gmail.com>
2020-12-11 14:07:32 -0500

    set up netlify functions

---
+++ functions/src/makePack.ts
@@ -0,0 +1,11 @@
+
+import { APIGatewayEvent, Context } from 'aws-lambda';
+
+export const handler = async (event: APIGatewayEvent, context: Context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      msg: 'Hello Netlify Functions'
+    })
+  }
+}
